b65abed4f61aa062db5542c8942893dcec3e2fb7
Ravenmaster9912 <jose.martinez6208@alumnos.udg.mx>
2024-11-07 22:08:41 -0600

    archivos

---
+++ helloworld/helloworld.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Imprimir en pantalla “Hello World”
+echo "Hello World"
+
+# Listar el contenido del fichero (directorio) actual
+ls
+
+# Crear un fichero (folder) llamado Test
+mkdir Test
+
+# Cambiarse al fichero Test
+cd Test
+
+# Listar el contenido del fichero Test
+ls
